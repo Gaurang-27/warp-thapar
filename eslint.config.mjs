8906f8d1296f4c3e79fad81ed86fac13d883c4c4
Gaurang-27 <gaurangg272@gmail.com>
2025-07-04 15:12:00 +0530

    unused-varoff

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules : {
+      // Turn off unused-vars completely
+      "@typescript-eslint/no-unused-vars": "off",
+      "no-unused-vars": "off", // also turn off base JS rule just in case
+    },
+  }
 ];
 
 export default eslintConfig;
